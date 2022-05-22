e68455d2f8211bfd1eab301c5d22de278b6bb58e
osadasami <osadasami@gmail.com>
2022-05-22 12:59:23 +0500

    fix query zero search result

--- main.ts
+++ main.ts
@@ -3,20 +3,27 @@ import { Plugin } from "obsidian";
 export default class SelectedSearchHighlight extends Plugin {
 	async onload() {
 		this.registerDomEvent(document, "click", (e: MouseEvent) => {
-			if (!app.workspace.getActiveFile()) {
+			if (
+				!app.workspace.getActiveFile() ||
+				document.querySelectorAll(
+					".search-results-children .search-result"
+				).length === 0
+			) {
 				return;
 			}
 
-			document.querySelectorAll(".search-result").forEach((el) => {
-				el.classList.remove("osadasami-search-result-active");
+			document
+				.querySelectorAll(".search-results-children .search-result")
+				.forEach((el) => {
+					el.classList.remove("osadasami-search-result-active");
 
-				if (
-					el.querySelector(".tree-item-inner").textContent ===
-					app.workspace.getActiveFile().basename
-				) {
-					el.classList.add("osadasami-search-result-active");
-				}
-			});
+					if (
+						el.querySelector(".tree-item-inner").textContent ===
+						app.workspace.getActiveFile().basename
+					) {
+						el.classList.add("osadasami-search-result-active");
+					}
+				});
 		});
 	}
 
